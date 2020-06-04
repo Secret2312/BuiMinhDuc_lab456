20f324deae187d1e53de1022de2ab0e35e3a8740
Administrator <Administrator@PC257>
2020-06-04 09:28:15 +0700

    First Commit. Them cac Controller va database

---
+++ BuiMinhDuc_Lab456/Controllers/CoursesController.cs
@@ -0,0 +1,29 @@
+﻿using BuiMinhDuc_Lab456.Models;
+using BuiMinhDuc_Lab456.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BuiMinhDuc_Lab456.Controllers
+{
+    public class CoursesController : Controller
+    {
+        private readonly ApplicationDbContext _dbContext;
+        public CoursesController()
+        {
+            _dbContext = new ApplicationDbContext();
+        }
+        // GET: Courses
+        [Authorize]
+        public ActionResult Create()
+        {
+            var viewModel = new CourseViewModel
+            {
+                Categories = _dbContext.Categories.ToList()
+            };
+            return View(viewModel);
+        }
+    }
+}
