cf00bf2627e95d73f527c4980967dac18b661ecc
Secret2312 <44581800+Secret2312@users.noreply.github.com>
2020-06-18 11:09:15 +0700

    complete

--- BuiMinhDuc_Lab456/Scripts/_references.js
+++ BuiMinhDuc_Lab456/Scripts/_references.js
@@ -1,5 +1,5 @@
-﻿/// <reference path="modernizr-2.8.3.js" />
-/// <reference path="jquery-3.5.1.js" />
+﻿/// <reference path="modernizr-2.6.2.js" />
+/// <reference path="jquery-1.10.2.js" />
 /// <autosync enabled="true" />
 /// <reference path="ai.0.22.9-build00167.min.js" />
 /// <reference path="bootstrap.js" />
